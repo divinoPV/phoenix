9c5fee416b55c368b0ea81f7de3d1fcbc7a380a6
divino <monteiro.hugo2001@icloud.com>
2022-02-28 23:11:51 +0100

    feature(#11)[create a fixture for each existing entity](FactFixtures)

---
+++ src/DataFixtures/FactFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Fact;
+use App\Enum\MilestoneEnum;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+final class FactFixtures extends BaseFixture implements DependentFixtureInterface
+{
+    public const REFERENCE = 'fact_';
+
+    public const NUMBER_ELEMENT = 145;
+
+    protected function generate(ObjectManager $manager): void
+    {
+        $this->create(Fact::class, self::NUMBER_ELEMENT, function (Fact $fact) {
+            $fact
+                ->setName(\implode(' ', $this->faker->words()))
+                ->setDescription(\implode(' ', $this->faker->sentences()))
+                ->setOccurred(new \DateTimeImmutable())
+                ->setProject($this->getReference(ProjectFixtures::REFERENCE . rand(1, ProjectFixtures::NUMBER_ELEMENT)))
+                ->setMilestone($this->getReference(MilestoneFixtures::REFERENCE . rand(0, count(MilestoneEnum::cases()) - 1)));
+        }, self::REFERENCE);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProjectFixtures::class,
+            MilestoneFixtures::class
+        ];
+    }
+}
