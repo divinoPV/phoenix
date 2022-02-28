9f25ec4d040d440f901a28d494584243e0d51539
divino <monteiro.hugo2001@icloud.com>
2022-02-28 23:14:27 +0100

    feature(#11)[create a fixture for each existing entity](UserFixtures)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,13 +2,21 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
-use App\Enum\RoleUser;
+use App\Enum\MemberTypeEnum;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\{Member, Responsible};
+use App\Enum\RoleUserEnum;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-final class UserFixtures extends BaseFixture
+final class UserFixtures extends BaseFixture implements DependentFixtureInterface
 {
+    public const REFERENCE_RESPONSIBLE = 'responsible_';
+
+    public const REFERENCE_MEMBER = 'member_';
+
+    public const NUMBER_ELEMENT = 124;
+
     public const PASSWORD = 'xxx';
 
     public function __construct(private UserPasswordHasherInterface $passwordHasher)
@@ -17,14 +25,32 @@ final class UserFixtures extends BaseFixture
 
     public function generate(ObjectManager $manager): void
     {
-        $this->create(User::class, 39, function (User $user) {
-            $user
-                ->setEmail($this->faker->email)
-                ->setName($this->faker->name)
-                ->setLastname($this->faker->lastName)
-                ->setUsername($this->faker->userName)
-                ->setRoles([RoleUser::cases()[rand(0, count(RoleUser::cases()) - 1)]])
-                ->setPassword($this->passwordHasher->hashPassword($user, self::PASSWORD));
-        });
+        $this->create(Responsible::class, self::NUMBER_ELEMENT, function (Responsible $responsible) {
+            $responsible
+                ->setEmail($this->faker->email())
+                ->setName($this->faker->name())
+                ->setLastname($this->faker->lastName())
+                ->setUsername($this->faker->userName())
+                ->setRoles([RoleUserEnum::cases()[rand(0, count(RoleUserEnum::cases()) - 1)]])
+                ->setPassword($this->passwordHasher->hashPassword($responsible, self::PASSWORD));
+        }, self::REFERENCE_RESPONSIBLE);
+
+        $this->create(Member::class, self::NUMBER_ELEMENT, function (Member $member) {
+            $member
+                ->setEmail($this->faker->email())
+                ->setName($this->faker->name())
+                ->setLastname($this->faker->lastName())
+                ->setUsername($this->faker->userName())
+                ->setRoles([RoleUserEnum::cases()[rand(0, count(RoleUserEnum::cases()) - 1)]])
+                ->setPassword($this->passwordHasher->hashPassword($member, self::PASSWORD))
+                ->setType($this->getReference(MemberTypeFixtures::REFERENCE . rand(0, count(MemberTypeEnum::cases()) - 1)));
+        }, self::REFERENCE_MEMBER);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            MemberTypeFixtures::class
+        ];
     }
 }
