001e6146cdf7272bec946a026209f2a28f764d24
divino <monteiro.hugo2001@icloud.com>
2022-04-02 11:46:12 +0200

    feature(#16)[create project routes, create project templates files]

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,16 +7,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/projects')]
+#[Route('/projets')]
 final class ProjectController extends AbstractController
 {
-    #[Route(name: 'project')]
+    #[Route(name: 'projets')]
     public function __invoke(ProjectRepository $projectRepository): Response
     {
-        dd(array_reduce(['2', '3', '4'], fn ($ax, $dx) => [...$ax, $dx], ['1']));
-
-        return $this->render('project/index.html.twig', [
+        return $this->render('app/project/archive.html.twig', [
             'projects' => $projectRepository->findAll(),
         ]);
+    }
+
+    #[Route('/projets/{id}', name: 'project')]
+    public function show(int $id, ProjectRepository $projectRepository): Response
+    {
+        return $this->render('app/project/show.html.twig', [
+            'project' => $projectRepository->find($id),
+        ]);
+    }
+
+    #[Route('/projets/ajouter', name: 'project_add')]
+    public function add(): Response
+    {
+        return $this->render('app/project/add.html.twig');
+    }
+
+    #[Route('/projets/{id}/editer', name: 'project_edit')]
+    public function edit(int $id, ProjectRepository $projectRepository): Response
+    {
+        return $this->render('app/project/edit.html.twig', [
+            'project' => $projectRepository->find($id),
+        ]);
+    }
+
+    #[Route('/projets/{id}/supprimer', name: 'project_delete')]
+    public function delete(int $id, ProjectRepository $projectRepository)
+    {
+    }
+
+    #[Route('/projets/exporter', name: 'projects_export')]
+    public function export(ProjectRepository $projectRepository)
+    {
     }
 }
