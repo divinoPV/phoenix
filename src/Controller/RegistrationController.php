ba6289f7c591ec1f97bae812a765418cf26b1a60
divino <monteiro.hugo2001@icloud.com>
2022-04-03 15:20:17 +0200

    feature(#26)[authentication]: /2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,35 +3,40 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Enum\UserRoleEnum;
 use App\Form\RegistrationFormType;
+use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
-#[Route('/register')]
+#[Route('/inscription')]
 final class RegistrationController extends AbstractController
 {
     #[Route(name: 'register')]
     public function __invoke(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
+        UserAuthenticatorInterface $userAuthenticator,
+        UserAuthenticator $authenticator,
         EntityManagerInterface $entityManager
     ): Response {
-        $form = $this->createForm(RegistrationFormType::class, $user = new User())->handleRequest($request);
+        $form = $this->createForm(RegistrationFormType::class, $user = new User)->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword($userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData()));
-            $entityManager->persist($user);
+            $entityManager->persist($user->setRoles(array_merge($user->getRoles(), [UserRoleEnum::Member->value])));
             $entityManager->flush();
 
-            return $this->redirectToRoute('dashboard');
+            return $userAuthenticator->authenticateUser($user, $authenticator, $request);
         }
 
-        return $this->renderForm('registration/register.html.twig', [
-            'registrationForm' => $form,
+        return $this->renderForm('registration/registration.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,10 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -18,32 +20,40 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', options: [
+            ->add('email', EmailType::class, [
                 'label' => 'user.email',
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                'label' => 'user.password',
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer une adresse email',
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
+                    new Email([
+                        'message' => 'Veuillez entrer une adresse email valide',
                     ]),
                 ],
             ])
+            ->add('plainPassword', PasswordType::class, [
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
+                        'max' => 63,
+                    ]),
+                ],
+                'label' => 'user.password',
+                'mapped' => false,
+            ])
             ->add('firstName', options: [
-                'label' => 'user.firstName',
+                'label' => 'user.first_name',
             ])
             ->add('lastName', options: [
-                'label' => 'user.lastName',
+                'label' => 'user.last_name',
             ])
             ->add('userName', options: [
-                'label' => 'user.userName',
+                'label' => 'user.user_name',
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'security.registration.submit'
